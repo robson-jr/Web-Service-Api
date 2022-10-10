96609a152c2fc84042d84c8b3a5d274614b3bd58
ERIVELTON DOS SANTOS BARRETO <erivelton.sbarreto@senacsp.edu.br>
2022-10-10 19:18:26 -0300

    first commit

---
+++ app/Models/Categoria.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Categoria extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nomedacategoria'];
+
+    protected $primaryKey = 'pkcategoria';
+
+    protected $table = 'categorias';
+
+    public $incrementing = true;
+
+    public $timestamps = false;
+}
